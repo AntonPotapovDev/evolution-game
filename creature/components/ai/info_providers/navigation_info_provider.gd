d351e7366154c2be47be71040ff5da6a1987dcd5
AntonPotapovDev <protestandprotect52@gmail.com>
2026-08-26 23:40:42 +0300

    Navigation info provider second iterration

--- creature/components/ai/info_providers/navigation_info_provider.gd
+++ creature/components/ai/info_providers/navigation_info_provider.gd
@@ -6,8 +6,10 @@ const WORLD_RADIUS: float = 5000.0
 const OFFSET: float = 64.0
 const NAVIGATION_RADIUS: float = WORLD_RADIUS - OFFSET
 
-const MIN_ANGLE_STEP: float = deg_to_rad(10.0)
-const MAX_ANGLE_STEP: float = deg_to_rad(20.0)
+const MIN_ANGLE_STEP: float = deg_to_rad(50.0)
+const MAX_ANGLE_STEP: float = deg_to_rad(60.0)
+
+const MAX_RADIUS_STEP: float = 1000.0
 
 
 var _creature: Creature
@@ -17,15 +19,24 @@ func _init(creature: Creature):
     _creature = creature
 
 
-func get_naviation_point() -> Vector2:
-    var current_angle = _get_angle_from_up(_creature.global_position)
+func get_navigation_point() -> Vector2:
+    var creature_pos = _creature.global_position
+    if creature_pos.is_zero_approx():
+        creature_pos = Vector2.RIGHT * 0.01
+
+    var current_angle = _get_angle_from_right(creature_pos)
     var next_angle = current_angle + randf_range(MIN_ANGLE_STEP, MAX_ANGLE_STEP)
-    var radius = randf_range(0.0, NAVIGATION_RADIUS)
-    return Vector2.from_angle(next_angle) * radius
+
+    var current_radius = creature_pos.distance_to(Vector2.ZERO)
+    var min_bound = max(current_radius - MAX_RADIUS_STEP, 0.0)
+    var max_bound = min(current_radius + MAX_RADIUS_STEP, NAVIGATION_RADIUS)
+    var next_radius = randf_range(min_bound, max_bound)
+
+    return Vector2.from_angle(next_angle) * next_radius
 
 
-func _get_angle_from_up(vector: Vector2) -> float:
-    var angle = Vector2.UP.angle_to(vector)
+func _get_angle_from_right(vector: Vector2) -> float:
+    var angle = Vector2.RIGHT.angle_to(vector)
     if angle < 0.0:
         angle += TAU
     return angle
