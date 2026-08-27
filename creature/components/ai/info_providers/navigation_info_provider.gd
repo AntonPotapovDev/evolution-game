8e0087ae292f41381c4945c9bbe033b504f1eb3b
AntonPotapovDev <protestandprotect52@gmail.com>
2026-08-28 02:21:07 +0300

    Navigation info provider final iterration

--- creature/components/ai/info_providers/navigation_info_provider.gd
+++ creature/components/ai/info_providers/navigation_info_provider.gd
@@ -14,25 +14,102 @@ const MAX_RADIUS_STEP: float = 1000.0
 
 var _creature: Creature
 
+var _angle_sign: int = 0
+var _points: Array[Vector2] = []
+
 
 func _init(creature: Creature):
     _creature = creature
 
 
-func get_navigation_point() -> Vector2:
-    var creature_pos = _creature.global_position
-    if creature_pos.is_zero_approx():
-        creature_pos = Vector2.RIGHT * 0.01
+func init_path():
+    var zero_point = _get_zero_point()
+    var first_point = _get_first_point(zero_point)
 
-    var current_angle = _get_angle_from_right(creature_pos)
-    var next_angle = current_angle + randf_range(MIN_ANGLE_STEP, MAX_ANGLE_STEP)
+    _update_angle_sign(zero_point, first_point)
 
-    var current_radius = creature_pos.distance_to(Vector2.ZERO)
-    var min_bound = max(current_radius - MAX_RADIUS_STEP, 0.0)
-    var max_bound = min(current_radius + MAX_RADIUS_STEP, NAVIGATION_RADIUS)
-    var next_radius = randf_range(min_bound, max_bound)
+    var second_point = _get_second_point(zero_point, first_point)
 
-    return Vector2.from_angle(next_angle) * next_radius
+    _points = [ first_point, second_point ] as Array[Vector2]
+
+
+func reset_path():
+    _angle_sign = 0
+    _points = []
+
+
+func get_next_point() -> Vector2:
+    if _points.size() > 0:
+        return _points.pop_front()
+
+    return _get_next_point()
+
+
+func _get_zero_point() -> Vector2:
+    var zero_point = _creature.global_position
+    if zero_point.is_zero_approx():
+        zero_point = Vector2.RIGHT * 0.1
+    return zero_point
+
+
+func _get_first_point(zero_point: Vector2) -> Vector2:
+    var radius = randf_range(0.0, NAVIGATION_RADIUS)
+    var angle = randf_range(0.0, TAU)
+    var first_point = Vector2.from_angle(angle) * radius
+
+    var distance = zero_point.distance_to(first_point)
+    if distance > MAX_RADIUS_STEP or distance < MAX_RADIUS_STEP / 2:
+        var direction = zero_point.direction_to(first_point)
+        var local_radius = randf_range(MAX_RADIUS_STEP / 2, MAX_RADIUS_STEP)
+        first_point = zero_point + local_radius * direction
+
+    return first_point
+
+
+func _get_second_point(zero_point: Vector2, first_point: Vector2) -> Vector2:
+    var angle = _get_next_point_angle(first_point)
+    var radius = _get_second_point_radius(zero_point, first_point)
+    return Vector2.from_angle(angle) * radius
+
+
+func _get_second_point_radius(zero_point: Vector2, first_point: Vector2) -> float:
+    var zero_point_radius = zero_point.distance_to(Vector2.ZERO)
+    var first_point_radius = first_point.distance_to(Vector2.ZERO)
+
+    var delta_sign = 1 if first_point_radius > zero_point_radius else -1
+
+    var min_bound = first_point_radius
+    var max_bound = clampf(first_point_radius + delta_sign * (MAX_RADIUS_STEP / 2), 0.0, NAVIGATION_RADIUS)
+
+    if min_bound > max_bound:
+        var temp = max_bound
+        max_bound = min_bound
+        min_bound = temp
+
+    return randf_range(min_bound, max_bound)
+
+
+func _get_next_point() -> Vector2:
+    var zero_point = _get_zero_point()
+
+    var zero_point_radius = zero_point.distance_to(Vector2.ZERO)
+
+    var min_bound = max(zero_point_radius - MAX_RADIUS_STEP, 0.0)
+    var max_bound = min(zero_point_radius + MAX_RADIUS_STEP, NAVIGATION_RADIUS)
+    var next_point_radius = randf_range(min_bound, max_bound)
+
+    var next_point_angle = _get_next_point_angle(zero_point)
+
+    return Vector2.from_angle(next_point_angle) * next_point_radius
+
+
+func _get_next_point_angle(prev_point: Vector2) -> float:
+    var prev_point_angle = _get_angle_from_right(prev_point)
+    return prev_point_angle + _angle_sign * randf_range(MIN_ANGLE_STEP, MAX_ANGLE_STEP)
+
+
+func _update_angle_sign(zero_point: Vector2, first_point: Vector2):
+    _angle_sign = 1 if zero_point.angle_to(first_point) > 0.0 else -1
 
 
 func _get_angle_from_right(vector: Vector2) -> float:
--- creature/components/ai/needs/feed_need/states/find_new_fields.gd
+++ creature/components/ai/needs/feed_need/states/find_new_fields.gd
@@ -2,17 +2,27 @@ class_name FindNewFieldsState
 extends AbstractFeedNeedState
 
 
+var _nip: NavigationInfoProvider
+var _current_target: Vector2 = Vector2.ZERO
+
+
 func _init(actor: Creature):
     super(actor)
+    _nip = NavigationInfoProvider.new(actor)
 
 
 func start():
-    pass
+    _nip.init_path()
+    _current_target = _nip.get_next_point()
 
 
 func update(delta: float):
-    _actor.movement.move_to_position(Vector2.ZERO, delta)
+    if _actor.global_position.distance_to(_current_target) < 64.0:
+        _current_target = _nip.get_next_point()
+
+    _actor.movement.move_to_position(_current_target, delta)
 
 
 func reset():
-    pass
+    _current_target = Vector2.ZERO
+    _nip.reset_path()
